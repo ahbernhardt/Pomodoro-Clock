2b7c5d2795853125f3ab0128b0e3e46dba47c28a
Anh Nguyen <a.nguyen0208@gmail.com>
2020-12-29 23:14:24 -0600

    Updates

---
+++ precache-manifest.59212bc2437ee7b15d7e6896e0e3e4bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618b211422be4e8bad35eae46aa92b2e",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "1df736be9cc3d7d07975",
+    "url": "/Pomodoro-Clock/static/css/main.0f7d6867.chunk.css"
+  },
+  {
+    "revision": "83b96f7d4a9f9452add5",
+    "url": "/Pomodoro-Clock/static/js/2.ea810944.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/Pomodoro-Clock/static/js/2.ea810944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df736be9cc3d7d07975",
+    "url": "/Pomodoro-Clock/static/js/main.bb6377d1.chunk.js"
+  },
+  {
+    "revision": "64d94c5cfed0b4294d93",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.0e3ceb2f.js"
+  },
+  {
+    "revision": "83c0a7e818da5770b49b8064f5b40ee9",
+    "url": "/Pomodoro-Clock/static/media/ding.83c0a7e8.mp3"
+  }
+]);
